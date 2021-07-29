cf3c148400cf9e45a4ac5d1e36538bce27bb2d7f
Anshul Khatri <59494777+khat3680@users.noreply.github.com>
2021-07-29 16:47:44 -0400

    header files

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/**
+ * 
+ * CP 386 - Assignment 4
+ * 
+ * @author Anshul Khatri (193313680) - https://github.com/khat3680/
+ * @author Pranav Verma () - https://github.com/Navsan1
+ * 
+ * @see https://github.com
+ * 
+ */
+#include <stdio.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <time.h>
