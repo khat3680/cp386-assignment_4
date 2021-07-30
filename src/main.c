3b97b772471a1e9b29a98b664f9189020456a5b1
Anshul Khatri <59494777+khat3680@users.noreply.github.com>
2021-07-30 11:09:35 -0400

    loading customer resources

--- src/main.c
+++ src/main.c
@@ -2,15 +2,10 @@
  * 
  * CP 386 - Assignment 4
  * 
-<<<<<<< HEAD
  * @author Anshul Khatri (193313680) 
  * Github login - https://github.com/khat3680/
  * @author Pranav Verma (193272030) 
  * Github login - https://github.com/Navsan1
-=======
- * @author Anshul Khatri (193313680) - https://github.com/khat3680/
- * @author Pranav Verma (193272030) - https://github.com/Navsan1
->>>>>>> 64eb88a8effbef36eceef9b4a768c69469c4e8c7
  * 
  * @see https://github.com/khat3680/cp386-assignment_4
  * 
@@ -31,9 +26,12 @@ typedef struct customer
     int *max_resources;
 } Customer;
 
-Customer *customer_resources;
+Customer *c_resources;
 
-int *avail_resources; //available array
+int n_resources;
+int n_customers;
+
+int *avail_resources; //available array for the resources
 
 /*main is where we will pass the commandline arguments for the filename 
 and we call readFile().
@@ -42,7 +40,6 @@ to runCommand function*/
 
 int main(int argc, char *argv[])
 {
-    char *fileName = "sample4_in.txt";
     // checking arguements
     if (argc < 3) //check if we have enough resources
     {
@@ -54,11 +51,88 @@ int main(int argc, char *argv[])
     {
         printf("\n\nWelcome to the program made by Anshul and Pranav. :D\n\n");
 
-        avail_resources = (int *)malloc((argc - 1) * sizeof(int)); // allocate memory for array
+        int n_resources = argc - 1;
 
-        for (int i = 1; i < argc - 1; i++) //put argv into available array
+        avail_resources = (int *)malloc((n_resources) * sizeof(int)); // allocate memory for array
+
+        for (int i = 1; i < n_resources; i++) //put argv into available array
         {
             avail_resources[i] = atoi(argv[i + 1]);
         }
     }
+
+    load_c_resources(); // Loads customer resource data from the file.
+}
+/** 
+ * Function to load customer resources. 
+ * 
+ * @author Anshul 
+ * @author Pranav
+ */
+
+int load_c_resources()
+{
+    // for reading from file
+    FILE *file_p;
+    char *line;
+    size_t len = 0;
+    ssize_t read;
+
+    // Open from file
+    if ((file_p = fopen("sample4_in.txt", "r")) == NULL)
+    {
+
+        printf("File opening error."); // Issue in opening file
+        return -1;
+    }
+
+    n_customers = 1; // start at one since last line does not have line break
+    // count number of lines in the file
+    char ch;
+    while ((ch = fgetc(file_p)) != EOF)
+    {
+        if (ch == '\n')
+        {
+            n_customers++;
+            // if line was actually empty then don't count it
+            if ((ch = fgetc(file_p)) == EOF)
+                n_customers--;
+        }
+    }
+    fseek(file_p, 0, SEEK_SET); // reset file_p back to start after counting
+
+    // allocate memory for customer resources data structure
+    c_resources = malloc(n_customers * sizeof(Customer));
+
+    // read customer data from file
+    int r, c = 0;
+    while ((read = getline(&line, &len, file_p)) != -1)
+
+    { // for each line in file
+        // ignore blank lines
+        if (strlen(line) > 1)
+        {
+            // create customer data structure to initialise it.
+            Customer customer;
+
+            customer.max_resources = delimited_string_to_int_array(line, ",", (n_resources));
+            customer.allocation_resources = malloc(sizeof(int) * n_resources);
+            customer.need_resources = malloc(sizeof(int) * n_resources);
+
+            // ensure no memory-related value issues occur by setting values to 0 (frick C)
+            for (r = 0; r < n_resources; r++)
+                customer.allocation_resources[r] = 0;
+            // need = max - allocation, allocation is all 0s now, so set to need = max to start
+
+            for (r = 0; r < n_resources; r++)
+                customer.need_resources[r] = customer.max_resources[r];
+
+            c_resources[c] = customer;
+            c++;
+        }
+    }
+
+    //avoid memory leaks
+    free(line);
+    fclose(file_p);
 }
